fc33cf85c095c8097d21ee8ff9a852cbb662eeb8
Israel Zorzor <46909353+Hammondziggy@users.noreply.github.com>
2023-03-18 22:58:41 +0000

    added root file for netlify

--- src/views/NotFoundPage.vue
+++ src/views/NotFoundPage.vue
@@ -1,6 +1,17 @@
 <template>
-  <div>
-    <h1>404 Not Found</h1>
-    <p>The page you are looking for does not exist.</p>
+  <div class="flex flex-col items-center justify-center h-screen">
+    <h1 class="text-3xl font-bold mb-4">404 Not Found</h1>
+    <p class="mb-8">The page you are looking for does not exist.</p>
+    <button class="px-4 py-2 rounded-md bg-blue-500 text-white font-bold" @click="redirectToHome">Go back to homepage</button>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    redirectToHome() {
+      this.$router.push('/');
+    }
+  }
+}
+</script>
